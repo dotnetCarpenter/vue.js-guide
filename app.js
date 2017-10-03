986e14d97637796d2e6327abcdbc2880df444b5d
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-10-03 21:20:11 +0200

    The data and the DOM are now linked, and everything is now reactive.

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ let app
 main()
 
 function main() {
-  fetchHtml('blocks/html/helloworld.vue')
+  fetchHtml('blocks/html/binding.vue')
     .then(insert('body'))
     .then(render)
+
+  // can not hover when entire text is changed each second TODO: fix vue.js
+  // setInterval(render, 1000)
 }
 
 function fetchHtml(path) {
@@ -25,9 +28,9 @@ function insert(selector) {
 
 function render() {
   app = new Vue({
-    el: '#app',
+    el: '#app-2',
     data: {
-      message: 'Hello Vue!'
+      message: 'You loaded this page on ' + new Date().toLocaleString()
     }
   })
 }
