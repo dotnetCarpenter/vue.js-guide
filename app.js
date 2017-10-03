72bfced91cba7a689493168bee5289f6a4a29004
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-10-03 21:59:16 +0200

    It’s easy to toggle the presence of an element

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 'use strict'
 
 let app
+let renderers = [
+  {
+    path: 'blocks/html/helloworld.vue',
+    render: () => {
+      app = new Vue({
+        el: '#app',
+        data: {
+          message: 'Hello Vue!'
+        }
+      })
+    }
+  }, {
+    path: 'blocks/html/binding.vue',
+    render: () => {
+      app = new Vue({
+        el: '#app-2',
+        data: {
+          message: 'You loaded this page on ' + new Date().toLocaleString()
+        }
+      })
+    }
+  } , {
+    path: 'blocks/html/toggle.vue',
+    render: () => {
+      app = new Vue({
+        el: '#app-3',
+        data: {
+          seen: true
+        }
+      })
+    }
+  }
+]
 
 // document.addEventListener('DOMContentLoaded', main)
-main()
+main(renderers[2])
 
-function main() {
-  fetchHtml('blocks/html/binding.vue')
+function main(config) {
+  fetchHtml(config.path)
     .then(insert('body'))
-    .then(render)
+    .then(config.render)
 
   // can not hover when entire text is changed each second TODO: fix vue.js
   // setInterval(render, 1000)
@@ -24,13 +57,4 @@ function insert(selector) {
       .querySelector('body')
       .innerHTML = html
   }
-}
-
-function render() {
-  app = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-  })
 }
---
+++ blocks/html/toggle.vue
@@ -0,0 +1,3 @@
+<div id="app-3">
+  <span v-if="seen" v-on:click="seen=false" style="cursor:pointer;">Now you see me</span>
+</div>
